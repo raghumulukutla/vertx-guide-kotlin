c262d92468e4dad452c7cb83d5a507b93e143a85
Raghu Mulukutla <raghu.tjm@gmail.com>
2019-03-22 14:45:40 -0400

    Gradle + Readme
    - gradle can now build and run individual steps via command line arguments
    - updated readme with instructions

---
+++ src/main/kotlin/com/raghu/vertx3guide/wiki/step2/HttpServerVerticle.kt
@@ -0,0 +1,10 @@
+package com.raghu.vertx3guide.wiki.step2
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+
+class HttpServerVerticle : AbstractVerticle() {
+  override fun start(startFuture: Future<Void>?) {
+    super.start(startFuture)
+  }
+}
